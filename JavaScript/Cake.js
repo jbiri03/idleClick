3ca3d4c08428ba18d31e521959c6ed7ec378c217
Jasmine Biri <jasminebiri03@gmail.com>
2026-06-16 19:23:39 -0700

    B. Added cake class

---
+++ JavaScript/Cake.js
@@ -0,0 +1,18 @@
+class Cake {
+	constructor (adjective, color, flavor, frosting){
+        this.adjective = adjective;
+		this.color = color;
+		this.flavor = flavor;
+		this.frosting = frosting;
+    }
+
+    displayInfo(){
+	return `${this.adjective} ${this.color} ${this.flavor} Cake with ${this.frosting} Frosting`;
+    }
+}
+
+const cakeDetails = document.getElementById("cakeDetails")
+const myCake = new Cake('Bold', 'Blue', 'Raspberry', 'Swiss Meringue');
+
+cakeDetails.textContent = myCake.displayInfo();
+
