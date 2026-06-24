9e19f63637de708396a8d5dea8f94ed38c46eaf8
Jasmine Biri <jasminebiri03@gmail.com>
2026-06-24 16:15:08 -0700

    B. Inserts data from user form

--- php/connect.php
+++ php/connect.php
@@ -6,11 +6,28 @@ require_once __DIR__ . '/config.php';
 // DEFINE VARIABLE THAT STORES DATA SOURCE NAME
 $dsn = "sqlite:$db";
 
+// VARIABLES TO CONNECT TO TABLE
+$email = $_POST['email'];
+$username = $_POST['username'];
+$table_name = 'users';
+
+//TRY CATCH
 try {
+    //CONNECT TO DB
     $pdo = new \PDO($dsn);
     echo 'Connected to the SQLite database successfully!' ;
+
+    //INSERT TABLE DATA
+    $sql = "INSERT INTO $table_name (email, username)
+    VALUES ('$email', '$username')";
+    $stmt = $pdo->prepare($sql);
+
+    //EXECUTE QUERY
+    $stmt->execute();
+
 } catch (\PDOException $e) {
     echo $e->getMessage();
 }
+
 
 ?>
